c9ae12eda056efe843b417dd8ed8c7873941e139
ravanabackup <sajintrack@gmail.com>
2024-09-07 09:58:34 +0530

    Update ipswitcher.bat

--- ipswitcher.bat
+++ ipswitcher.bat
@@ -10,20 +10,25 @@ echo 4. Exit
 set /p choice="Enter your choice (1, 2, 3, or 4): "
 
 if "%choice%"=="1" (
+    title Office DNS Selected
     netsh interface ip set address name="Wi-Fi" static 10.xxx.xxx.xx 255.255.255.0 10.xxx.xxx.1
     netsh interface ip set dns name="Wi-Fi" static xxx.xxx.x.1
     netsh interface ip add dns name="Wi-Fi" xxx.xxx.xx.3 index=2
     echo Office network settings applied.
 ) else if "%choice%"=="2" (
+    title Reset DNS Selected
     netsh interface ip set address name="Wi-Fi" dhcp
     netsh interface ip set dns name="Wi-Fi" dhcp
     echo DNS settings have been reset to default.
 ) else if "%choice%"=="3" (
+    title Network and Sharing Center Opened
     control.exe /name Microsoft.NetworkAndSharingCenter
     echo Network and Sharing Center opened.
 ) else if "%choice%"=="4" (
+    title Exiting...
     exit
 ) else (
+    title Invalid Choice
     echo Invalid choice. Please select 1, 2, 3, or 4.
 )
 
